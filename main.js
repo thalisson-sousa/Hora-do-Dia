5fff95a6fd29d967c32aa050b2feec45a7fec0d3
thalisson-sousa <thalissonpop91@gmail.com>
2023-03-28 18:27:14 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+function carregar() {
+const msg = document.querySelector('#msg')
+const img = document.querySelector('#foto')
+const data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} Horas.`
+
+if (hora >= 0 && hora < 12) {
+    img.innerHTML = '<p>Bom dia!</p>' + '<img src="img/fotomanha.png" alt="foto do dia">'
+    document.body.style.background = 'rgb(70, 142, 236)'
+} else if (hora < 18) {
+    img.innerHTML = '<p>Boa Tarde!</p>' + '<img src="img/fototarde.png" alt="foto do tarde">'
+    document.body.style.background = 'orange'
+} else {
+    img.innerHTML = '<p>Boa Noite!</p>' + '<img src="img/fotonoite.png" alt="foto da noite">'
+    document.body.style.background = 'black'
+}
+}
